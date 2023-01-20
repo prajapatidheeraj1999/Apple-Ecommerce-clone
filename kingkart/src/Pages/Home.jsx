472a874d9c2bf12a7d7a514b15919f827189dca7
https://github.com/masai-course/dheeraj_prajapati_fw21_1005.git <dheerajprajapatijnp@gmail.com>
2023-01-20 20:01:01 +0530

    day04

--- kingkart/src/Pages/Home.jsx
+++ kingkart/src/Pages/Home.jsx
@@ -1,13 +1,60 @@
+import { Box, Button, Center, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import Footer from '../Component/Footer'
-
+import frist from "../images/image 1.png"
+import second from "../images/image 2.png"
+import third from "../images/image 3.png"
+import fourth from "../images/image 4.png"
+import five from "../images/image 5.png"
+import six from "../images/image 6.png"
+import CaptionCarousel from "./HomepageCoregel"
 
 function Home() {
   return (
-    <div>
-      <h1>Home page we need to work on page</h1>
-      <Footer/>
-    </div>
+
+    <>
+      <Box bg="#000000" color="white" pt={40}>
+        <Center>
+          <Text fontSize='5xl'>Introducing the new </Text>
+        </Center>
+        <Text fontSize='5xl'>MacBook Pro and Mac mini.</Text>
+        <Button colorScheme='teal' variant='outline' mt={5} mb={20}>
+          Watch the announcement
+        </Button>
+        <Image src={frist} alt='frist'></Image>
+      </Box>
+      <Box mt={3}>
+        <Center>
+          <Image src={second} alt='Second'></Image>
+        </Center>
+      </Box>
+      <Box mt={3}>
+        <Center>
+          <Image src={third} alt='Second'></Image>
+        </Center>
+      </Box>
+      <Box mt={3}>
+        <Center>
+          <Image src={fourth} alt='Second'></Image>
+        </Center>
+      </Box>
+      <Box mt={3}>
+        <Center>
+          <Image src={five} alt='Second'></Image>
+        </Center>
+      </Box>
+      <Box mt={3} mb={3}>
+        <Center>
+          <Image src={six} alt='Second'></Image>
+        </Center>
+      </Box>
+      <CaptionCarousel/>
+
+      <Footer />
+    </>
+
+
+
   )
 }
 
